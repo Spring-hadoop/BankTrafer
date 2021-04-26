4fda45b91e6ec7ff7cddd2cdf006bacf052a6098
Spring-hadoop <729165621@qq.com>
2021-04-27 01:58:43 +0800

    first commit

---
+++ src/main/java/com/plus/bysj/account/beans/req/TransactionConfirmReq.java
@@ -0,0 +1,16 @@
+package com.plus.bysj.account.beans.req;
+
+import lombok.Data;
+
+/**
+ * @ProjectName: account
+ * @Package: com.plus.bysj.account.beans.req
+ * @ClassName: TransactionConfirmReq
+ * @Author: rh
+ * @Description:
+ * @Date: 2021/4/24 22:46
+ */
+@Data
+public class TransactionConfirmReq {
+    private String transactionId;
+}
